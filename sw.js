9c0fd35773348fc0f73626e4907ac88e8978e121
katyaadaricheva-dev <katyaadaricheva@gmail.com>
2026-07-03 19:54:26 +0300

    Add files via upload

---
+++ sw.js
@@ -0,0 +1,18 @@
+const CACHE = 'planner-v1';
+const URLS = [ './', './index.html' ];
+
+self.addEventListener('install', e => {
+  e.waitUntil(caches.open(CACHE).then(c => c.addAll(URLS)));
+  self.skipWaiting();
+});
+
+self.addEventListener('activate', e => {
+  e.waitUntil(caches.keys().then(ks => Promise.all(ks.map(k => { if (k !== CACHE) return caches.delete(k); }))));
+  self.clients.claim();
+});
+
+self.addEventListener('fetch', e => {
+  e.respondWith(
+    caches.match(e.request).then(r => r || fetch(e.request).catch(() => new Response('', { status: 200 })))
+  );
+});
